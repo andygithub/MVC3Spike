c7253ace7ac5285f095c364b7e2f389e451f0526
andy <andymaurer07@gmail.com>
2011-02-17 20:52:42 -0500

    Initial load.
    Signed-off-by: andy <andymaurer07@gmail.com>

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+This is a group of projects that deal with various and features and processes for the asp.net MVC 3 release.
+
+SimpleMVC3 - This folder contains minor modifications to the example here: http://www.asp.net/mvc/tutorials/getting-started-with-mvc3-part1-cs
---
+++ SimpleMVC3/SimpleMVC3/Models/Movie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SimpleMVC3.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+        
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Genre of Movie must be specified")]
+        [DisplayName("Genre of Movie")] public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Price Required")]
+        [Range(1, 100, ErrorMessage = "Price must be between $1 and $100")]
+        public decimal Price { get; set; }
+
+        [StringLength(5)]
+        public string Rating { get; set; }
+
+
+    }
+    
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().Property(p => p.Price).HasPrecision(18, 2);
+        }
+
+    }
+
+}
